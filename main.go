5c756ffde3e3a34406a30b2b92798e198cb3c472
dark7lord <dark7lord@yandex.ru>
2026-07-26 00:07:16 +0500

    feat: dockerize app and add Sentry error monitoring

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"os"
+	"time"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(sentrygin.New(sentrygin.Options{Repanic: false}))
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
+		panic("I love Haruno Sakura")
 	})
 
 	return router
@@ -20,10 +27,23 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn == "" {
+		log.Fatal("SENTRY_DSN is not set")
+	}
+
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: dsn,
+	}); err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	r.GET("/debug-sentry", func(c *gin.Context) {
+		panic("test sentry capture")
 	})
 
 	if err := r.Run(":8080"); err != nil {
